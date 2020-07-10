6b7f5bda21c52be65f660effc8b4f99876f33d78
Stefan Cameron <stefan@stefcameron.com>
2020-07-10 17:35:36 -0500

    Reproduce the bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    `gatsby-plugin-mdx`,
+  ],
 }
